f77395a5fcfc4ac5171c10f3ee71a17cef428344
iqranathani <iqra.arif.nathani@gmail.com>
2020-03-15 02:42:41 +0500

    changes

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -9,9 +9,18 @@ class Channels extends Component {
         modal: false
     }
 
+    handleSubmit = event => {
+        event.preventDefault();
+        if (this.isFormValid(this.state)) {
+            console.log('channel added');
+        }
+    }
+
     handleChange = event => {
         this.setState({ [event.target.name]: event.target.value });
     };
+
+    isFormValid = ( channelName, channelDetails ) => channelName && channelDetails;
 
     openModal = () => this.setState({ modal: true });
     closeModal = () => this.setState({ modal: false });
@@ -35,7 +44,7 @@ class Channels extends Component {
             <Modal basic open={modal} onClose={this.closeModal}>
                 <Modal.Header>Add a Channel</Modal.Header>
                     <Modal.Content>
-                        <Form>
+                        <Form onSubmit={this.handleSubmit}>
                             <Form.Field>
                                 <Input
                                     fluid
@@ -57,7 +66,7 @@ class Channels extends Component {
                     </Modal.Content>
 
                     <Modal.Actions>
-                        <Button color="green" inverted>
+                        <Button color="green" inverted onClick={this.handleSubmit}>
                             <Icon name="checkmark" /> Add
                         </Button>
 
